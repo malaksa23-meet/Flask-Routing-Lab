65d41e7c6a8e0be2321daba9e7efc465d7b6c335
malaksa23-meet <109904323+malaksa23-meet@users.noreply.github.com>
2022-07-26 16:53:09 +0300

    .

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -7,8 +7,18 @@ app = Flask(__name__,
     )
 
 @app.route('/')
-def fr():
+def home():
     return render_template("home.html")
+
+@app.route('/product')
+def product():
+    return render_template("product.html")
+
+
+@app.route('/cart ')
+def cart ():
+    return render_template("cart.html")
+
 
 
 
